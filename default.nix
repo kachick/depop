2e2a161eac2de13c57574bc2b3b20446ea66f75d
Kenichi Kamiya <kachick1@gmail.com>
2023-07-03 15:17:44 +0900

    `nix-headbump bump`

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/e57b65abbbf7a2d5786acc86fdf56cde060ed026.tar.gz") { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/87fbcab2cde3bf5d35a3f80cc3ec7179b9425e1e.tar.gz") { } }:
 
 pkgs.mkShell {
   buildInputs = [
