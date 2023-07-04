f89925298606ff7ba5b05c49bdbfc8599060b234
Kenichi Kamiya <kachick1@gmail.com>
2023-07-04 21:37:49 +0900

    Implement option to hide "Sponsors" (#16)
    * Implement to hide sponsors
    
    * Remove transpiled JS
    
    * Provide option to hide sponsors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
-const exploreRepositoriesSideBar = document.querySelector(
-  "div[aria-label='Explore repositories']",
-);
-
-if (exploreRepositoriesSideBar) {
-  chrome.storage.sync.get([
-    "isHideExploreRepositories",
-  ]).then((keys) => {
+const hide = (element: Element): void => {
+  element.setAttribute(
     // Prefer hidden rather than display:none https://primer.style/css/utilities/layout#the-html-hidden-attribute
-    if (keys.isHideExploreRepositories) {
-      exploreRepositoriesSideBar.setAttribute(
-        "hidden",
-        // Both `true` and `false` will be interpreted as `true`.
-        // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/hidden
-        "true",
-      );
+    "hidden",
+    // Both `true` and `false` will be interpreted as `true`.
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/hidden
+    "true",
+  );
+};
+
+chrome.storage.sync.get([
+  "isHideExploreRepositories",
+  "isHideSponsors",
+]).then((keys): void => {
+  if (keys.isHideExploreRepositories) {
+    const exploreRepositoriesComponent = document.querySelector(
+      "div[aria-label='Explore repositories']",
+    );
+
+    if (exploreRepositoriesComponent) {
+      hide(exploreRepositoriesComponent);
     }
-  });
-}
+  }
+
+  if (keys.isHideSponsors) {
+    const sponsorsH2Node = document.evaluate(
+      "/html/body//div[@class='Layout-sidebar']//h2[text()='Sponsors']",
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null,
+    ).singleNodeValue;
+
+    const sponsorsComponent = sponsorsH2Node?.parentElement?.parentElement;
+
+    if (sponsorsComponent) {
+      hide(sponsorsComponent);
+    }
+  }
+});
