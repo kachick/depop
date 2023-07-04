36a2c2dbd4f5459642c5db233fabd12e976948b9
Kenichi Kamiya <kachick1@gmail.com>
2023-07-05 00:21:08 +0900

    WIP - ZIP

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,3 +1,5 @@
+import { compress } from 'https://deno.land/x/zip@v1.2.5/mod.ts';
+
 const version = JSON.parse(await Deno.readTextFile('manifest.json')).version;
 if (typeof version !== 'string') {
   throw new Error('Version is not found in manifest.json');
@@ -14,23 +16,17 @@ const distFiles = Array.from(Deno.readDirSync('dist')).flatMap((filename) =>
   filename.isFile ? [`dist/${filename.name}`] : []
 );
 
-// Avoid to name `.crx`. See https://developer.chrome.com/docs/webstore/publish/ for further detail
-const command = new Deno.Command('zip', {
-  args: [
-    product,
-    'manifest.json',
-    'README.md',
-    'LICENSE',
-    'assets/icon-sadness-star.png',
-    ...staticFiles,
-    ...distFiles,
-  ],
-});
-const result = command.outputSync();
+console.log(Deno.cwd());
+const p2 = Deno.cwd() + '/out/archive.zip';
 
-if (result.success) {
-  console.log(`Built to ${product}: \n${decoder.decode(result.stdout)}`);
+// Avoid to name `.crx`. See https://developer.chrome.com/docs/webstore/publish/ for further detail
+const success = await compress([
+  'README.md',
+], p2);
+
+if (success) {
+  console.log(`Built to ${product}`);
 } else {
-  console.error(`Failed to build: \n${decoder.decode(result.stderr)}`);
-  Deno.exit(result.code);
+  console.error('Failed to build');
+  Deno.exit(1);
 }
