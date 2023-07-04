16bb5c526f76db89d4a97f03f3476e920f9064f7
Kenichi Kamiya <kachick1@gmail.com>
2023-07-05 02:36:11 +0900

    fflate is great!

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,32 +1,32 @@
-import { compress } from 'https://deno.land/x/zip@v1.2.5/mod.ts';
+// @deno-types="https://cdn.skypack.dev/-/fflate@v0.8.0-9lwslwAa54POmYjLobRf/dist=es2019,mode=raw/lib/index.d.ts"
+import * as fflate from 'https://cdn.skypack.dev/fflate@0.8.0?min';
 
 const version = JSON.parse(await Deno.readTextFile('manifest.json')).version;
 if (typeof version !== 'string') {
   throw new Error('Version is not found in manifest.json');
 }
-const product = `out/depop-${version}.zip`;
 
-const decoder = new TextDecoder();
+const paths = [
+  'manifest.json',
+  'README.md',
+  'LICENSE',
+  'assets/icon-sadness-star.png',
+  ...Array.from(Deno.readDirSync('static')).flatMap((
+    dirEntity,
+  ) => dirEntity.isFile ? [`static/${dirEntity.name}`] : []),
+  ...Array.from(Deno.readDirSync('dist')).flatMap((dirEntity) =>
+    dirEntity.isFile ? [`dist/${dirEntity.name}`] : []
+  ),
+];
 
-const staticFiles = Array.from(Deno.readDirSync('static')).flatMap((
-  dirEntity,
-) => dirEntity.isFile ? [`static/${dirEntity.name}`] : []);
+const productPath = `out/depop-${version}.zip`;
 
-const distFiles = Array.from(Deno.readDirSync('dist')).flatMap((dirEntity) =>
-  dirEntity.isFile ? [`dist/${dirEntity.name}`] : []
+Deno.writeFileSync(
+  productPath,
+  fflate.zipSync(paths.reduce(
+    (acc, path) => ({ ...acc, [path]: Deno.readFileSync(path) }),
+    {},
+  )),
 );
 
-console.log(Deno.cwd());
-const p2 = Deno.cwd() + '/out/archive.zip';
-
-// Avoid to name `.crx`. See https://developer.chrome.com/docs/webstore/publish/ for further detail
-const success = await compress([
-  'README.md',
-], p2);
-
-if (success) {
-  console.log(`Built to ${product}`);
-} else {
-  console.error('Failed to build');
-  Deno.exit(1);
-}
+console.log(`Built to ${productPath}:`, paths);
