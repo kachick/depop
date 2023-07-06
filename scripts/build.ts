d37de3ece39398634a6a696e8aea2820386e9be1
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 18:36:07 +0900

    Print the size

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,7 @@
 // @deno-types="https://cdn.skypack.dev/-/fflate@v0.8.0-9lwslwAa54POmYjLobRf/dist=es2019,mode=raw/lib/index.d.ts"
 import * as fflate from 'https://cdn.skypack.dev/fflate@0.8.0?min';
 import { toHashString } from 'https://deno.land/std@0.193.0/crypto/to_hash_string.ts';
+import prettyBytes from 'https://esm.sh/pretty-bytes@6.1.0';
 
 const version = JSON.parse(await Deno.readTextFile('manifest.json')).version;
 if (typeof version !== 'string') {
@@ -50,6 +51,7 @@ console.log(
       productPath,
       productBasename,
       productSha256,
+      productSize: prettyBytes(zipped.length),
       structure: Array.from(zipStructure.entries()).reduce(
         (acc, [path, { sha256 }]) => ({ ...acc, [path]: sha256 }),
         {},
