45b46f0d996edce40498cce01844b3ddfb2f711b
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 18:09:03 +0900

    Always require --allow-net in bundler

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,3 +1,4 @@
+// Always require --allow-net https://github.com/denoland/deno_emit/issues/81
 import { bundle } from 'https://deno.land/x/emit@0.24.0/mod.ts';
 import { parse } from 'https://deno.land/std@0.193.0/flags/mod.ts';
 
