33582804028043b7fb99659b7df777920499378a
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 21:10:06 +0900

    Avoid StrictMode for now...

--- src/options.tsx
+++ src/options.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'https://esm.sh/react@18.2.0';
+import React from 'https://esm.sh/react@18.2.0';
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
 import App from './options/App.tsx';
 import { assertIsDefined } from './typeguards.ts';
@@ -7,7 +7,7 @@ const root = document.getElementById('root');
 assertIsDefined<HTMLElement | null>(root);
 
 createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // FIX ME: <StrictMode><App /></StrictMode> can NOT be used with following type error
+  // error: TS2786 [ERROR]: 'StrictMode' cannot be used as a JSX component.
+  <App />,
 );
