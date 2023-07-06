964ee49468e9c40f1d10a41ab6759ca134561c37
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 15:25:17 +0900

    Print Hash list after build scripts

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,6 @@
 // @deno-types="https://cdn.skypack.dev/-/fflate@v0.8.0-9lwslwAa54POmYjLobRf/dist=es2019,mode=raw/lib/index.d.ts"
 import * as fflate from 'https://cdn.skypack.dev/fflate@0.8.0?min';
+import { toHashString } from 'https://deno.land/std@0.193.0/crypto/to_hash_string.ts';
 
 const version = JSON.parse(await Deno.readTextFile('manifest.json')).version;
 if (typeof version !== 'string') {
@@ -20,13 +21,34 @@ const includesPaths = [
 ];
 
 const productPath = `out/depop-${version}.zip`;
+const zipStructure = new Map<string, { content: Uint8Array; sha256: string }>();
+
+for (const path of includesPaths) {
+  const content = Deno.readFileSync(path);
+  const digest = await crypto.subtle.digest('SHA-256', content);
+  zipStructure.set(path, { content, sha256: toHashString(digest, 'base64') });
+}
 
 Deno.writeFileSync(
   productPath,
-  fflate.zipSync(includesPaths.reduce(
-    (acc, path) => ({ ...acc, [path]: Deno.readFileSync(path) }),
-    {},
-  )),
+  fflate.zipSync(
+    Array.from(zipStructure.entries()).reduce(
+      (acc, [path, { content }]) => ({ ...acc, [path]: content }),
+      {},
+    ),
+  ),
 );
 
-console.log(JSON.stringify({ productPath, includesPaths }, undefined, 4));
+console.log(
+  JSON.stringify(
+    {
+      productPath,
+      structure: Array.from(zipStructure.entries()).reduce(
+        (acc, [path, { sha256 }]) => ({ ...acc, [path]: sha256 }),
+        {},
+      ),
+    },
+    undefined,
+    4,
+  ),
+);
