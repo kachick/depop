f2a1a3d76a333c8f1b5b9ba6eef716b414a9e154
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 21:18:54 +0900

    Revert to avoid console.log

--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -8,7 +8,7 @@ if (typeof entrypoint !== 'string') {
   throw new Error('Need to specify entrypoint');
 }
 
-const result = await bundle(entrypoint);
+const result = await bundle(new URL(entrypoint, import.meta.url));
 
 const { code } = result;
-Deno.stdout.writeSync(new TextEncoder().encode(code));
+console.log(code);
