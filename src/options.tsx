5e70d9b3a7c6a4f4629a122ef858ba926fa75cd5
Kenichi Kamiya <kachick1@gmail.com>
2023-07-06 17:02:06 +0900

    Suppress deno typecheck error for ReactNode with JSX

--- src/options.tsx
+++ src/options.tsx
@@ -1,5 +1,3 @@
-/// <reference lib="dom"/>
-
 import React, { StrictMode } from 'https://esm.sh/react@18.2.0';
 import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client';
 import App from './options/App.tsx';
@@ -7,8 +5,5 @@ import { assertIsDefined } from './typeguards.ts';
 
 const root = document.getElementById('root');
 assertIsDefined<HTMLElement | null>(root);
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+
+createRoot(root).render(StrictMode({ children: <App /> }));
