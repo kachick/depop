b61aaee71f3f6363c8aeee7e4d4923e8e9d39d57
Kenichi Kamiya <kachick1@gmail.com>
2023-07-12 19:10:28 +0900

    Add sha256 prefix for build zip to easy understand

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,9 @@
 import { toHashString } from 'https://deno.land/std@0.193.0/crypto/to_hash_string.ts';
-import { basename, extname } from 'https://deno.land/std@0.193.0/path/posix.ts';
+import {
+  basename,
+  extname,
+  join,
+} from 'https://deno.land/std@0.193.0/path/posix.ts';
 // Always require --allow-net https://github.com/denoland/deno_emit/issues/81
 import { bundle } from 'https://deno.land/x/emit@0.24.0/mod.ts';
 // @deno-types="https://cdn.skypack.dev/-/fflate@v0.8.0-9lwslwAa54POmYjLobRf/dist=es2019,mode=raw/lib/index.d.ts"
@@ -41,7 +45,7 @@ const gatherDist = Promise.all([
   Deno.copyFile('vendor/primer.css', 'dist/primer.css'),
   Deno.copyFile(
     'assets/icon-sadness-star.png',
-    'dist/assets/icon-sadness-star.png',
+    'dist/icon-sadness-star.png',
   ),
 ]);
 
@@ -49,34 +53,27 @@ await gatherDist;
 
 const getOrderedPathList = (dirPath: string): string[] =>
   Array.from(Deno.readDirSync(dirPath)).flatMap((dirEntity) =>
-    dirEntity.isFile ? [`${dirPath}/${dirEntity.name}`] : []
+    dirEntity.isFile ? [dirEntity.name] : []
   ).toSorted();
 
-const productBasename = `depop-${version}.zip`;
-const productPath = `dist/${productBasename}`;
 const zipStructure = new Map<
   string,
   { content: Uint8Array; sha256: string; isCompressed: boolean }
 >();
 
-const setZipStructure = async (
-  zip: typeof zipStructure,
-  paths: Readonly<string[]>,
-  isCompressed: boolean,
-): Promise<void> => {
-  for (const path of paths) {
-    const content = Deno.readFileSync(path);
-    const digest = await crypto.subtle.digest('SHA-256', content);
-    zip.set(path, {
-      content,
-      sha256: toHashString(digest),
-      isCompressed,
-    });
-  }
+const sha256 = async (content: Uint8Array) => {
+  const digest = await crypto.subtle.digest('SHA-256', content);
+  return toHashString(digest);
 };
 
-await setZipStructure(zipStructure, getOrderedPathList('dist'), false);
-await setZipStructure(zipStructure, getOrderedPathList('dist/assets'), true);
+for (const path of getOrderedPathList('dist')) {
+  const content = Deno.readFileSync(join('dist', path));
+  zipStructure.set(path, {
+    content,
+    sha256: await sha256(content),
+    isCompressed: extname(path) === '.png',
+  });
+}
 
 const zipped = fflate.zipSync(
   Array.from(zipStructure.entries()).reduce(
@@ -88,10 +85,15 @@ const zipped = fflate.zipSync(
   ),
 );
 
-const productSha256 = toHashString(
-  await crypto.subtle.digest('SHA-256', zipped),
+const structure = Array.from(zipStructure.entries()).reduce(
+  (acc, [path, { sha256 }]) => ({ ...acc, [path]: sha256 }),
+  {},
 );
-
+const structureSha256 = await sha256(
+  new TextEncoder().encode(JSON.stringify(structure)),
+);
+const productBasename = `depop-${version}-${structureSha256.slice(0, 7)}.zip`;
+const productPath = `dist/${productBasename}`;
 Deno.writeFileSync(productPath, zipped);
 
 console.log(
@@ -99,12 +101,10 @@ console.log(
     {
       productPath,
       productBasename,
-      productSha256,
+      structureSha256,
+      structure,
       productSize: prettyBytes(zipped.length),
-      structure: Array.from(zipStructure.entries()).reduce(
-        (acc, [path, { sha256 }]) => ({ ...acc, [path]: sha256 }),
-        {},
-      ),
+      productSha256: await sha256(zipped),
     },
     undefined,
     4,
