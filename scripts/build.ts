cc39519ba7bb82057bb9f76ec31145228482150d
Kenichi Kamiya <kachick1@gmail.com>
2023-07-13 03:25:52 +0900

    Add validator in build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,20 +4,21 @@ import {
   extname,
   join,
 } from 'https://deno.land/std@0.193.0/path/posix.ts';
+import manifestJson from '../src/manifest.json' assert {
+  type: 'json',
+};
+import manifestSchema from '../src/manifestSchemaAdjusted.json' assert {
+  type: 'json',
+};
+import { assertEquals } from 'https://deno.land/std@0.193.0/testing/asserts.ts';
 // Always require --allow-net https://github.com/denoland/deno_emit/issues/81
 import { bundle } from 'https://deno.land/x/emit@0.24.0/mod.ts';
 // @deno-types="https://cdn.skypack.dev/-/fflate@v0.8.0-9lwslwAa54POmYjLobRf/dist=es2019,mode=raw/lib/index.d.ts"
 import * as fflate from 'https://cdn.skypack.dev/fflate@0.8.0?min';
+import Ajv from 'https://esm.sh/ajv@8.12.0';
 import prettyBytes from 'https://esm.sh/pretty-bytes@6.1.0';
 
-const manifestPath = 'src/manifest.json';
-
-const version = JSON.parse(await Deno.readTextFile(manifestPath)).version;
-if (typeof version !== 'string') {
-  throw new Error('Version is not found in manifest.json');
-}
-
-const cleanUp = () => {
+const cleanUpSync = () => {
   try {
     Deno.removeSync('./dist', { recursive: true });
   } catch (error) {
@@ -29,18 +30,21 @@ const cleanUp = () => {
   Deno.mkdirSync('dist/assets', { recursive: true });
 };
 
-cleanUp();
+cleanUpSync();
 
-for (const entryTsPath of ['src/github-patcher.ts', 'src/options.tsx']) {
-  const bundled = await bundle(entryTsPath);
-  const outputPath = `dist/${basename(entryTsPath, extname(entryTsPath))}.js`;
-  Deno.writeTextFileSync(outputPath, bundled.code);
-}
+const transpile = async () => {
+  for (const entryTsPath of ['src/github-patcher.ts', 'src/options.tsx']) {
+    const bundled = await bundle(entryTsPath);
+    const outputPath = `dist/${basename(entryTsPath, extname(entryTsPath))}.js`;
+    Deno.writeTextFileSync(outputPath, bundled.code);
+  }
+};
 
 const gatherDist = Promise.all([
+  transpile(),
   Deno.copyFile('README.md', 'dist/README.md'),
   Deno.copyFile('LICENSE', 'dist/LICENSE'),
-  Deno.copyFile(manifestPath, 'dist/manifest.json'),
+  Deno.copyFile('src/manifest.json', 'dist/manifest.json'),
   Deno.copyFile('src/github-patcher.css', 'dist/github-patcher.css'),
   Deno.copyFile('src/options.html', 'dist/options.html'),
   Deno.copyFile('src/options.css', 'dist/options.css'),
@@ -87,28 +91,63 @@ const zipped = fflate.zipSync(
   ),
 );
 
+const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
+const toString = (bytes: Uint8Array) => textDecoder.decode(bytes);
+
 const structure = Array.from(zipStructure.entries()).reduce(
   (acc, [path, { sha256 }]) => ({ ...acc, [path]: sha256 }),
   {},
 );
 const structureSha256 = await sha256(
-  new TextEncoder().encode(JSON.stringify(structure)),
+  textEncoder.encode(JSON.stringify(structure)),
 );
-const productBasename = `depop-${version}-${structureSha256.slice(0, 7)}.zip`;
+const productBasename = `depop-${manifestJson.version}-${
+  structureSha256.slice(0, 7)
+}.zip`;
 const productPath = `dist/${productBasename}`;
 Deno.writeFileSync(productPath, zipped);
 
-console.log(
-  JSON.stringify(
-    {
-      productPath,
-      productBasename,
-      structureSha256,
-      structure,
-      productSize: prettyBytes(zipped.length),
-      productSha256: await sha256(zipped),
-    },
-    undefined,
-    4,
-  ),
-);
+const validateProduct = async (zipped: Uint8Array) => {
+  const unzipped = fflate.unzipSync(zipped);
+
+  assertEquals(JSON.parse(toString(unzipped['manifest.json'])), manifestJson);
+
+  const ajv = new Ajv({ allErrors: true });
+  const schemaOk = await ajv.validate(manifestSchema, manifestJson);
+
+  const definedPaths = [
+    ...manifestJson.content_scripts.flatMap(({ css, js }) => [...css, ...js]),
+    manifestJson.options_page,
+    ...Object.values(manifestJson.icons),
+  ];
+  const includedPaths = new Set(Object.keys(unzipped));
+  // Except assets like css from options.html
+  const missingPaths = definedPaths.filter((path) => !includedPaths.has(path));
+
+  const ok = schemaOk && missingPaths.length === 0;
+
+  return [ok, { schemaErrors: ajv.errors, missingPaths }];
+};
+
+const [ok, errors] = await validateProduct(zipped);
+
+if (ok) {
+  console.log(
+    JSON.stringify(
+      {
+        productPath,
+        productBasename,
+        structureSha256,
+        structure,
+        productSize: prettyBytes(zipped.length),
+        productSha256: await sha256(zipped),
+      },
+      undefined,
+      4,
+    ),
+  );
+} else {
+  console.error(errors);
+  Deno.exit(1);
+}
