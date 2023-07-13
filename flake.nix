ecf55492455c53f90ee2685f2de84f9fdb0fa26a
Kenichi Kamiya <kachick1@gmail.com>
2023-07-13 12:56:41 +0900

    Use Nix flakes (#37)
    * Use Nix flakes
    
    * Add gitignore for direnv with flkae
    
    * Update CLI options for nix develop era

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/87fbcab2cde3bf5d35a3f80cc3ec7179b9425e1e";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nil
+              nixpkgs-fmt
+              dprint
+              imagemagick
+              deno
+              actionlint
+              ripgrep
+            ];
+          };
+      });
+}
