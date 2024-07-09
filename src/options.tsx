3ee2349c2d84eb7155ebf85042031cb5cf68995e
Kenichi Kamiya <kachick1@gmail.com>
2024-07-09 13:51:43 +0900

    Suppress deno-lint no-unused-vars

--- src/options.tsx
+++ src/options.tsx
@@ -1,6 +1,6 @@
 /** @jsxImportSource https://esm.sh/v128/react@18.2.0 */
 
-import React, { StrictMode } from 'https://esm.sh/v128/react@18.2.0';
+import { StrictMode } from 'https://esm.sh/v128/react@18.2.0';
 import { createRoot } from 'https://esm.sh/v128/react-dom@18.2.0/client';
 import App from './options/App.tsx';
 import { assertIsDefined } from './typeguards.ts';
