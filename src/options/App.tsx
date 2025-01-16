485cf80a2506c1e258ca70f0db81453938cd780a
Kenichi Kamiya <kachick1@gmail.com>
2025-01-16 12:54:09 +0900

    Bump to react 19 with specifying es2022 forn esm.sh target
    If not adding this or adding denonext, it failed with following error
    
    error: Uncaught (in promise) Error: Unable to output during bundling: load_transformed failed: failed to analyze module: failed to resolve node:process from https://esm.sh/react@19.0.0/denonext/react.mjs: Cannot resolve "node:process

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,6 +1,6 @@
 // deno-lint-ignore no-unused-vars
-import * as React from 'https://esm.sh/react@18.3.1'; // Load whole `React` to avoid no reference, if omitting, should check options page manually
-import { useEffect, useState } from 'https://esm.sh/react@18.3.1';
+import * as React from 'https://esm.sh/react@19.0.0?target=es2022'; // Load whole `React` to avoid no reference, if omitting, should check options page manually
+import { useEffect, useState } from 'https://esm.sh/react@19.0.0?target=es2022';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
