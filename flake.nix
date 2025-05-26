b6feb51f2613fd7bbc97dbba9740ee571eb99e5d
Kenichi Kamiya <kachick1@gmail.com>
2025-05-26 14:07:27 +0900

    Uninstall selfup to reduce build time for devshell
    No binary cache is hard for Nix Life

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    selfup = {
-      url = "github:kachick/selfup/v1.1.7";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      selfup,
     }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
@@ -25,23 +20,21 @@
         in
         {
           default = pkgs.mkShellNoCC {
-            buildInputs =
-              (with pkgs; [
-                bashInteractive
-                nil
-                nixfmt-rfc-style
+            buildInputs = with pkgs; [
+              bashInteractive
+              nil
+              nixfmt-rfc-style
 
-                deno
-                stylelint
-                dprint
-                ripgrep
-                typos
+              deno
+              stylelint
+              dprint
+              ripgrep
+              typos
 
-                # To maintain icons
-                imagemagick
-                exiftool
-              ])
-              ++ [ selfup.packages.${system}.default ];
+              # To maintain icons
+              imagemagick
+              exiftool
+            ];
           };
         }
       );
